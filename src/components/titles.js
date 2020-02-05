7def0931350fd61d18e44e448c1e0e1435a59baa
akbel <colakakbel@gmail.com>
2020-02-06 00:55:57 +0100

    open a title component

---
+++ src/components/titles.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import App from '../App';
+
+class Titles extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Weather Finder</h1>
+                <p>Is it Cold? Find out why...</p>
+            </div>
+        );
+    }
+};
+
+export default Titles;
