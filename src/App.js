f54d93aa19a0f67575eea29258ec2c9156b05d26
akbel <colakakbel@gmail.com>
2020-02-05 23:51:57 +0100

    till 17th minute, create react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello!</h1>
+        <p> whats going on? </p>
+      </div>
+    );
+  }
+}
+
+export default App;
