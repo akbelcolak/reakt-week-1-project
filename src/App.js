ac7d5835cce24bea0efa724fc139adf9d64a95a0
akbel <colakakbel@gmail.com>
2020-02-06 12:59:22 +0100

    added getWeather,api_call: Mench/uk, data not used yet

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import Titles from './components/titles';
 import Form from './components/form';
 import Weather from './components/weather';
 
+const API_KEY = 'df2359151f0eb16fc94aa4855cd11fb5';
+
 class App extends React.Component {
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    
+  }
   render() {
     return (
       <div>
